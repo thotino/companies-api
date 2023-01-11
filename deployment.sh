2d2c5402bbb8b101c3c832c0a2e1a25b46f6b08a
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2023-01-11 16:52:17 +0100

    feat: updating the deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,12 +1,19 @@
 echo "start"
+
 kubectl delete -f ./kubernetes/deployment.yaml
 kubectl delete -f ./kubernetes/service.yaml
+kubectl delete -f ./kubernetes/autoscaler.yaml
+kubectl delete -f ./kubernetes/ingress.yaml
 docker-compose build
 minikube image load companies-api:latest
 sleep 10
 kubectl apply -f ./kubernetes/deployment.yaml
 kubectl apply -f ./kubernetes/service.yaml
+kubectl apply -f ./kubernetes/autoscaler.yaml
+kubectl apply -f ./kubernetes/ingress.yaml
 sleep 10
 kubectl get deployments
 kubectl get services
+kubectl get ingresses
+minikube service companies-service --url
 echo "end"
